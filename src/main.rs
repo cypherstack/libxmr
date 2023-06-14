1eb7e9fe264778cbd19839433726cf19b4fd1f95
Josh Babb <sneurlax@gmail.com>
2023-06-14 16:47:23 -0500

    example showing seed generation
    TODO put this into its own function and wrap it to over to Dart

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 // This file mostly exists in order to just functionality test via `cargo run`
 
 use monero_serai::{
-    random_scalar,
-    rpc::{HttpRpc, Rpc},
+    // random_scalar,
+    // rpc::{HttpRpc, Rpc},
     wallet::{
-        ViewPair, Scanner,
-        address::{AddressError, Network, AddressType, AddressSpec, AddressMeta, MoneroAddress},
-        SpendableOutput,
+        // ViewPair, Scanner,
+        // address::{AddressError, Network, AddressType, AddressSpec, AddressMeta, MoneroAddress},
+        // SpendableOutput,
         seed::{
             Seed, Language
         },
     },
 };
+// use monero_serai::*;
+
+use rand_core::OsRng; // for generating a seed
 
 fn main() {
-    let name = "world";
-    println!("Hello, {}!", name);
+    let seed = Seed::new(&mut OsRng, Language::English);
+    println!("{:?}", Seed::to_string(&seed));
 }
