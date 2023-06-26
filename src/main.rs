941c8f1353085f5a43cf44ee3a07d8315d95ee0d
Josh Babb <sneurlax@gmail.com>
2023-06-26 17:35:52 -0500

    add address.to_string() prints
    TODO fix test vector public address

--- src/main.rs
+++ src/main.rs
@@ -39,13 +39,13 @@ fn main() {
     let view: [u8; 32] = Keccak256::digest(&spend).into();
     println!("Private view key: {:?}", hex::encode(view));
     let view_point: EdwardsPoint = hash_to_point(view);
-    // println!("{:?}", 
-    //         MoneroAddress::new(
-    //           AddressMeta::new(Network::Mainnet, AddressType::Standard),
-    //           spend_point,
-    //           view_point,
-    //         ),
-    //     );
+    let address = MoneroAddress::new(
+              AddressMeta::new(Network::Mainnet, AddressType::Standard),
+              spend_point,
+              view_point,
+            );
+    println!("Public address: {:?}", address.to_string());
+
 
     println!("\nRunning checks on test vector...");
     // run same tests on test vector; see https://xmrtests.llcoins.net/addresstests.html
@@ -53,15 +53,25 @@ fn main() {
     // seed (hex): 29adefc8f67515b4b4bf48031780ab9d071d24f8a674b879ce7f245c37523807
     // private spend: 29adefc8f67515b4b4bf48031780ab9d071d24f8a674b879ce7f245c37523807
     // private view: 3bc0b202cde92fe5719c3cc0a16aa94f88a5d19f8c515d4e35fae361f6f2120e
+    // public spend: 72170da1793490ea9d0243df46c515444c35104b92b1d75a7d8c5954ba1f49cd
+    // public view: 21243cb8d0046baf10619d1fe7f38708095b006ef8e8350963c160478c1c0ff0
+    // address: 45wsWad9EwZgF3VpxQumrUCRaEtdyyh6NG8sVD3YRVVJbK1jkpJ3zq8WHLijVzodQ22LxwkdWx7fS2a6JzaRGzkNU8K2Dhi
     let mnemonic = "hemlock jubilee eden hacksaw boil superior inroads epoxy exhale orders cavernous second brunt saved richly lower upgrade hitched launching deepest mostly playful layout lower eden";
     let seed = Seed::from_string(Zeroizing::new(mnemonic.to_string())).unwrap();
     println!("Seed (mnemonic): {:?}", Seed::to_string(&seed));
 
+    // generate address from test vector seed above
     let spend: [u8; 32] = *seed.entropy();
     println!("Private spend key: {:?}", hex::encode(spend));
     let spend_point: EdwardsPoint = hash_to_point(spend);
     let view: [u8; 32] = Keccak256::digest(&spend).into();
     let view_scalar = Scalar::from_bytes_mod_order(view);
     println!("Private view key: {:?}", hex::encode(view_scalar.to_bytes()));
-    let view_point: EdwardsPoint = hash_to_point(view_scalar.to_bytes()); // TODO should view or view_scalar be used here? we'll find out through the process of address encoding
+    let view_point: EdwardsPoint = hash_to_point(view); // TODO should view or view_scalar.to_bytes() be used here? we'll find out through the process of address encoding
+    let address = MoneroAddress::new(
+              AddressMeta::new(Network::Mainnet, AddressType::Standard),
+              spend_point,
+              view_point,
+            );
+    println!("Public address: {:?}", address.to_string());
 }
