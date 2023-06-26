4314ab8e49b44d61d53eda2a71f4388983a954d7
Josh Babb <sneurlax@gmail.com>
2023-06-26 13:09:26 -0500

    show example of generating address in rust
    TODO extract spend and view keys from Seed

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,25 @@ use monero_serai::{
         // ViewPair, Scanner,
         // address::{AddressError, Network, AddressType, AddressSpec, AddressMeta, MoneroAddress},
         // SpendableOutput,
-        seed::{
-            Seed, Language
-        },
+        seed::{Seed, Language},
+        address::{AddressType, AddressMeta, MoneroAddress, Network},
     },
 };
 // use monero_serai::*;
 
 use rand_core::OsRng; // for generating a seed
 
+use curve25519_dalek::constants::ED25519_BASEPOINT_POINT; // for generating an address TODO remove after generating address from Seed vs basepoint
+
 fn main() {
     println!("{:?}", Seed::to_string(&Seed::new(&mut OsRng, Language::English)));
+
+    // generate address
+    println!("{:?}", 
+            MoneroAddress::new(
+              AddressMeta::new(Network::Mainnet, AddressType::Standard),
+              ED25519_BASEPOINT_POINT, // TODO spend key from Seed above
+              ED25519_BASEPOINT_POINT, // TODO view key from Seed above
+            ),
+        );
 }
