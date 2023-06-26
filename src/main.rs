b51617dcd92026e3e9efd8d2232a1c0e227f308c
Josh Babb <sneurlax@gmail.com>
2023-06-26 16:14:26 -0500

    show that the generated spend key is correct
    TODO verify if and why the view key is wrong (or if it's just different but nonetheless valid)

--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,43 @@ use sha3::{Digest, Keccak256}; // for generating the view key
 
 use monero_generators::{hash_to_point}; // for [u8; 32] -> EdwardsPoint conversion
 
-fn main() {
-    // generate seed & return as mnemonic
-    let seed = &Seed::new(&mut OsRng, Language::English); // TODO input lang as param
-    println!("{:?}", Seed::to_string(seed));
+use zeroize::Zeroizing;
 
-    // generate address
+fn main() {
+    println!("Running generation check...");
+    // generate random seed & print as mnemonic
+    let seed = &Seed::new(&mut OsRng, Language::English); // TODO input lang as param
+    println!("Seed (mnemonic): {:?}", Seed::to_string(seed));
+
+    // generate address from seed generated above
     let spend: [u8; 32] = *seed.entropy();
+    println!("Private spend key: {:?}", hex::encode(spend));
     let spend_point: EdwardsPoint = hash_to_point(spend);
     let view: [u8; 32] = Keccak256::digest(&spend).into();
+    println!("Private view key: {:?}", hex::encode(view));
     let view_point: EdwardsPoint = hash_to_point(view);
-    println!("{:?}", 
-            MoneroAddress::new(
-              AddressMeta::new(Network::Mainnet, AddressType::Standard),
-              spend_point,
-              view_point,
-            ),
-        );
+    // println!("{:?}", 
+    //         MoneroAddress::new(
+    //           AddressMeta::new(Network::Mainnet, AddressType::Standard),
+    //           spend_point,
+    //           view_point,
+    //         ),
+    //     );
+
+    println!("\nRunning checks on test vector...");
+    // run same tests on test vector; see https://xmrtests.llcoins.net/addresstests.html
+    // seed (mnemonic): hemlock jubilee eden hacksaw boil superior inroads epoxy exhale orders cavernous second brunt saved richly lower upgrade hitched launching deepest mostly playful layout lower eden
+    // seed (hex): 29adefc8f67515b4b4bf48031780ab9d071d24f8a674b879ce7f245c37523807
+    // private spend: 29adefc8f67515b4b4bf48031780ab9d071d24f8a674b879ce7f245c37523807
+    // private view: 3bc0b202cde92fe5719c3cc0a16aa94f88a5d19f8c515d4e35fae361f6f2120e
+    let mnemonic = "hemlock jubilee eden hacksaw boil superior inroads epoxy exhale orders cavernous second brunt saved richly lower upgrade hitched launching deepest mostly playful layout lower eden";
+    let seed = Seed::from_string(Zeroizing::new(mnemonic.to_string())).unwrap();
+    println!("Seed (mnemonic): {:?}", Seed::to_string(&seed));
+
+    let spend: [u8; 32] = *seed.entropy();
+    println!("Private spend key: {:?}", hex::encode(spend));
+    let spend_point: EdwardsPoint = hash_to_point(spend);
+    let view: [u8; 32] = Keccak256::digest(&spend).into();
+    println!("Private view key: {:?}", hex::encode(view));
+    let view_point: EdwardsPoint = hash_to_point(view);
 }
